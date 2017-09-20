ac7040a1d2150226fe304ce51019ff71f7329445
Barto Molina <bartomolina@gmail.com>
2017-09-19 21:47:38 -0400

    initial commit

---
+++ client/store.js
@@ -0,0 +1,27 @@
+import { createStore } from 'redux'
+
+const GOT_MESSAGES_FROM_SERVER = 'GOT_MESSAGES_FROM_SERVER'
+
+const initialState = {
+    messages: []
+}
+
+export const gotMessagesFromServer = function (array) {
+    return {
+        type: GOT_MESSAGES_FROM_SERVER,
+        messages: array
+    }
+}
+
+function reducer(state = initialState, action) {
+    switch (action.type) {
+        case GOT_MESSAGES_FROM_SERVER:
+            return Object.assign({}, state, { messages: action.messages })
+
+        default:
+            return state
+    }
+}
+
+const store = createStore(reducer)
+export default store
